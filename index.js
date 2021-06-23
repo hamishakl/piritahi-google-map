b457ff3ef9155e49654befe14bd192f53278c184
Hamish Henare <hamishjustin7@gmail.com>
2021-06-23 14:28:15 +1200

    testing zoom level on webflow site

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function initialize() {
       mapTypeIds: [google.maps.MapTypeId.TERRAIN, MY_MAPTYPE_ID],
     },
     mapTypeId: MY_MAPTYPE_ID,
-    zoom: 6,
+    zoom: 8,
     center: { lat: -36.84, lng: 174.76 },
     disableDefaultUI: true,
   };

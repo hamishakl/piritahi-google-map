0d2c0b1719393c2affb12fc0299f332677c2c5d1
hamishakl <hamishjustin7@gmail.com>
2022-02-25 12:24:59 +1300

    changed roskillsouth zoom

--- maps/roskillsouth.js
+++ maps/roskillsouth.js
@@ -20,7 +20,7 @@ function initMap() {
       strictBounds: false,
     }
   });
-  var opt = { minZoom: 15, maxZoom: 16 };
+  var opt = { minZoom: 15, maxZoom: 18 };
   map.setOptions(opt);
 
   var myCoordinates = [
